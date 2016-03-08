c53f45746daf2a45adc85c47e9fcf53430ee76cd
RDimon2912 <rdimon2912@gmail.com>
2016-03-08 21:59:52 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+
+using namespace std;
+
+int n, a[100500];
+
+int main() {
+	scanf ("%d", &n);
+	for (int i = 1; i <= n; i ++)
+		scanf ("%d", &a[i]);
+	sort (a + 1, a + 1 + n);
+	for (int i = 1; i <= n; i ++) 
+		printf ("%d ", a[i]);
+	return 0;	
+}
